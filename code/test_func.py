efda1361bf52edb115a280638dfe664a5be59871
Nirav Shah <nshah28@ncsu.edu>
2022-08-27 11:26:51 -0400

    Test case

---
+++ code/test_func.py
@@ -0,0 +1,24 @@
+import arithmatic
+'''import qrcreator'''
+
+def test_calc_addition():
+    output = arithmatic.add(2,4)
+    assert output == 6
+ 
+def test_calc_substraction():
+    output = arithmatic.sub(12, 4)
+    assert output == 8
+     
+def test_calc_multiply():
+    output = arithmatic.mul(6,4)
+    assert output == 24
+
+def test_calc_divide():
+    output = arithmatic.div(10,2)
+    assert output == 5
+'''
+
+def test_qr():
+    qrcreator.parametrize("data")
+    print("Passes")
+'''
