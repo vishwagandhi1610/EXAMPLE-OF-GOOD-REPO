89bc46051c5723c12b8419ea613bb9b5ebf511c2
VrushankiPatel <111774275+VrushankiPatel@users.noreply.github.com>
2022-08-27 11:14:24 -0400

    Add files via upload

---
+++ code/arithmatic.py
@@ -0,0 +1,15 @@
+# Addition
+def add(a, b):
+    return a+b
+
+# Subtraction
+def sub(a, b):
+    return a-b
+
+# Multiplication
+def mul(a, b):
+    return a*b
+
+# Division
+def div(a, b):
+    return a/b
