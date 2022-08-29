5c8eff314f6af873325760f53100b120af4cb0a9
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-08-28 23:54:49 -0400

    Restructured code into a package

---
+++ code/arithmatic/__init__.py
@@ -0,0 +1,4 @@
+from .functions import add, sub, mul, div
+
+
+__all__ = ['add', 'sub', 'mul', 'div']
---
+++ code/arithmatic/functions.py
@@ -0,0 +1,18 @@
+# Addition
+def add(a, b):
+    return a + b
+
+
+# Subtraction
+def sub(a, b):
+    return a - b
+
+
+# Multiplication
+def mul(a, b):
+    return a * b
+
+
+# Division
+def div(a, b):
+    return a / b
